879bf3ac1b59ef86b9f8a2f9e7e74c521a9a35cc
Samukle ♪ <31776410+Samukle@users.noreply.github.com>
2025-11-06 20:29:48 -0300

    feat: básico
    Alterações mínima de JavaScript...

---
+++ root/login.js
@@ -0,0 +1,5 @@
+// MongoDB início de sessão
+// ----------------------
+
+ var a = document.getElementById();
+// Imagino que tem que implementar isso aqui no Form sem ter que colocar o evento manualmente!
---
+++ root/logout.js
@@ -0,0 +1,2 @@
+// MongoDB final de sessão (botão superior direito no main.html)
+// ----------------------
