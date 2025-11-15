8a254be9769ef4a933ece5e1fb754bd41411a0fb
Samukle ♪ <31776410+Samukle@users.noreply.github.com>
2025-11-15 18:23:47 -0300

    feat: quick image fix

--- root/header.php
+++ root/header.php
@@ -1,6 +1,6 @@
 <header>
 	<nav id="navbar">
-		<img src="" width="30" height="30" alt="">
+		<img src="../asset/image/devicon.svg" width="30" height="30" alt="">
 		<?php
 			if (!empty($_SESSION)) {
 				echo '<form action="../db/logout.php" method="POST"><button type="submit" name="submit">';
